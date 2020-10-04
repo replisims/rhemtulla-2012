0c226d9593830ad8314b8d5287dab09238a7e151
Arjan <ArjanHJHuizing@gmail.com>
2020-10-04 17:16:10 +0200

    Add functions for data generation

---
+++ R/import.R
@@ -0,0 +1,6 @@
+#' @importFrom dplyr       arrange filter group_by
+#'                         mutate recode select
+#'                         tibble ungroup %>% .data
+#' @importFrom simsem      bind binds bindDist model generate
+#' @importFrom stats       sd
+NULL
