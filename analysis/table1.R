2b3c89cf61b2ce876648ffa2ecc05a2500727da6
Huizing <arjan.huizing@tno.nl>
2022-05-14 23:12:16 +0200

    Create table 1

--- analysis/table1.R
+++ analysis/table1.R
@@ -2,8 +2,26 @@
 # Skew and Kurtosis of Simulated data -------------------------------------
 
 library(moments)
+library(rhemtulla2012)
+library(tidyverse)
 
-moments::skewness(data)
+sim_scenarios <- rhemtulla2012:::getScenarios(its = 1,
+                                              N = 100000,
+                                              cat = c(2, 3, 4, 5, 6, 7),
+                                              sym = c("sym", "moderate", "extreme", "moderate-alt", "extreme-alt" ),
+                                              models = c("model1"),
+                                              dist = c("normal", "non-normal"),
+                                              conditions = NULL)
 
-kurtosis(data)
+sim_scenarios %>% 
+  rowwise() %>%
+  mutate(cat_data = list(getCatData(models = models,
+                                    N = N, 
+                                    cat = cat, 
+                                    sym = sym, 
+                                    dist = dist)[, 1])) -> simDat
 
+simDat$skew <- simDat$cat_data %>%
+  map_dbl(skewness) # good
+simDat$kurt <- simDat$cat_data %>%
+  map_dbl(kurtosis) # bad
