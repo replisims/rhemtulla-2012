1751d149c8b0e5293ae2b6cb7ed7c0bdefa72ffa
Anna Lohmann <mail@annloh.net>
2022-06-08 23:36:12 +0200

    Finalize Report

--- analysis/heat_plot.R
+++ analysis/heat_plot.R
@@ -66,9 +66,155 @@ A7_dif <- ggplot(table_dif_long7, aes(x = factor(cell, levels = paste0("X",2:41)
         panel.grid.major.y=element_blank(), 
         panel.grid.minor.y=element_blank(),
         panel.background=element_rect(fill="white"), # background=white
-        axis.text.x = element_text(angle=90, hjust = 1,vjust=1,size = 12,face = "bold"),
+        axis.text.x = element_text(angle=90, hjust = 1,vjust=1,size = 10,face = "bold"),
         plot.title = element_text(size=20,face="bold"),
-        axis.text.y = element_text(size = 12,face = "bold")) + 
+        axis.text.y = element_text(size = 10,face = "bold")) + 
+  theme(legend.title=element_text(face="bold", size=14)) + 
+  scale_x_discrete(name="") +
+  scale_y_reverse()+
+  labs(fill="Absolute Diff.")
+
+
+# Table A8 ----------------------------------------------------------------
+
+
+A8 <- read_table2("dump/A8.csv", col_names = FALSE)
+A8 <- A8[,-42]
+
+table_dif8 <- A8[,-1]-fig_a8_dat_wide[,-c(1,2)]
+
+
+table_dif_long8 <- pivot_longer(data = tibble(row = 1:nrow(table_dif8), table_dif8),
+                                cols = !row,
+                                values_to = "value",
+                                names_to = "cell")
+
+
+A8_dif <- ggplot(table_dif_long8, aes(x = factor(cell, levels = paste0("X",2:41)), 
+                                      y = row)) + # x and y axes => Var1 and Var2
+  geom_tile(aes(fill = value)) + # background colours are mapped according to the value column
+  geom_text(aes(fill = value, label = round(value, digits = 3)), size = 2) + # write the values
+  scale_fill_gradient2(low = "darkred", 
+                       mid = "white", 
+                       high = "midnightblue", 
+                       midpoint = 0) + # determine the colour
+  theme(panel.grid.major.x=element_blank(), #no gridlines
+        panel.grid.minor.x=element_blank(), 
+        panel.grid.major.y=element_blank(), 
+        panel.grid.minor.y=element_blank(),
+        panel.background=element_rect(fill="white"), # background=white
+        axis.text.x = element_text(angle=90, hjust = 1,vjust=1,size = 10,face = "bold"),
+        plot.title = element_text(size=20,face="bold"),
+        axis.text.y = element_text(size = 10,face = "bold")) + 
+  theme(legend.title=element_text(face="bold", size=14)) + 
+  scale_x_discrete(name="") +
+  scale_y_reverse()+
+  labs(fill="Absolute Diff.")
+
+
+# Table A9 ----------------------------------------------------------------
+A9 <- read_table2("dump/A9.csv", col_names = FALSE)
+A9 <- A9[,-42]
+
+table_dif9 <- A9[,-1]-fig_a9_dat_wide[,-c(1,2)]
+
+
+table_dif_long9 <- pivot_longer(data = tibble(row = 1:nrow(table_dif9), table_dif9),
+                                cols = !row,
+                                values_to = "value",
+                                names_to = "cell")
+
+
+A9_dif <- ggplot(table_dif_long9, aes(x = factor(cell, levels = paste0("X",2:41)), 
+                                      y = row)) + # x and y axes => Var1 and Var2
+  geom_tile(aes(fill = value)) + # background colours are mapped according to the value column
+  geom_text(aes(fill = value, label = round(value, digits = 3)), size = 2) + # write the values
+  scale_fill_gradient2(low = "darkred", 
+                       mid = "white", 
+                       high = "midnightblue", 
+                       midpoint = 0) + # determine the colour
+  theme(panel.grid.major.x=element_blank(), #no gridlines
+        panel.grid.minor.x=element_blank(), 
+        panel.grid.major.y=element_blank(), 
+        panel.grid.minor.y=element_blank(),
+        panel.background=element_rect(fill="white"), # background=white
+        axis.text.x = element_text(angle=90, hjust = 1,vjust=1,size = 10,face = "bold"),
+        plot.title = element_text(size=20,face="bold"),
+        axis.text.y = element_text(size = 10,face = "bold")) + 
+  theme(legend.title=element_text(face="bold", size=14)) + 
+  scale_x_discrete(name="") +
+  scale_y_reverse()+
+  labs(fill="Absolute Diff.")
+
+
+# Table A10 ---------------------------------------------------------------
+
+
+A10 <- read_table2("dump/A10.csv", col_names = FALSE)
+A10 <- A10[,-42]
+
+table_dif10 <- A10[,-1]-fig_a10_dat_wide[,-c(1,2)]
+
+
+table_dif_long10 <- pivot_longer(data = tibble(row = 1:nrow(table_dif10), table_dif10),
+                                cols = !row,
+                                values_to = "value",
+                                names_to = "cell")
+
+
+A10_dif <- ggplot(table_dif_long10, aes(x = factor(cell, levels = paste0("X",2:41)), 
+                                      y = row)) + # x and y axes => Var1 and Var2
+  geom_tile(aes(fill = value)) + # background colours are mapped according to the value column
+  geom_text(aes(fill = value, label = round(value, digits = 3)), size = 2) + # write the values
+  scale_fill_gradient2(low = "darkred", 
+                       mid = "white", 
+                       high = "midnightblue", 
+                       midpoint = 0) + # determine the colour
+  theme(panel.grid.major.x=element_blank(), #no gridlines
+        panel.grid.minor.x=element_blank(), 
+        panel.grid.major.y=element_blank(), 
+        panel.grid.minor.y=element_blank(),
+        panel.background=element_rect(fill="white"), # background=white
+        axis.text.x = element_text(angle=90, hjust = 1,vjust=1,size = 10,face = "bold"),
+        plot.title = element_text(size=20,face="bold"),
+        axis.text.y = element_text(size = 10,face = "bold")) + 
+  theme(legend.title=element_text(face="bold", size=14)) + 
+  scale_x_discrete(name="") +
+  scale_y_reverse()+
+  labs(fill="Absolute Diff.")
+
+
+# Table A11 ---------------------------------------------------------------
+
+
+A11 <- read_table2("dump/A11.csv", col_names = FALSE)
+A11 <- A11[,-42]
+
+table_dif11 <- A11[,-1]-fig_a11_dat_wide[,-c(1,2)]
+
+
+table_dif_long11 <- pivot_longer(data = tibble(row = 1:nrow(table_dif11), table_dif11),
+                                 cols = !row,
+                                 values_to = "value",
+                                 names_to = "cell")
+
+
+A11_dif <- ggplot(table_dif_long11, aes(x = factor(cell, levels = paste0("X",2:41)), 
+                                        y = row)) + # x and y axes => Var1 and Var2
+  geom_tile(aes(fill = value)) + # background colours are mapped according to the value column
+  geom_text(aes(fill = value, label = round(value, digits = 3)), size = 2) + # write the values
+  scale_fill_gradient2(low = "darkred", 
+                       mid = "white", 
+                       high = "midnightblue", 
+                       midpoint = 0) + # determine the colour
+  theme(panel.grid.major.x=element_blank(), #no gridlines
+        panel.grid.minor.x=element_blank(), 
+        panel.grid.major.y=element_blank(), 
+        panel.grid.minor.y=element_blank(),
+        panel.background=element_rect(fill="white"), # background=white
+        axis.text.x = element_text(angle=90, hjust = 1,vjust=1,size = 10,face = "bold"),
+        plot.title = element_text(size=20,face="bold"),
+        axis.text.y = element_text(size = 10,face = "bold")) + 
   theme(legend.title=element_text(face="bold", size=14)) + 
   scale_x_discrete(name="") +
   scale_y_reverse()+
--- analysis/tables_supplement_no_outliers.R
+++ analysis/tables_supplement_no_outliers.R
@@ -3,6 +3,24 @@
 
 sim_fit_cov <- readRDS("sim_fit_cov_no_out.rds")
 
+sim_fit_all_unnest <- readRDS("sim_fit_all_unnest_no_out.rds")
+
+
+
+lambda_03 <- c('b1', 'b6', 'b11', 'b16') 
+lambda_04 <- c('b2', 'b7', 'b12', 'b17')
+lambda_05 <- c('b3', 'b8', 'b13', 'b18')
+lambda_06 <- c('b4', 'b9', "b14", "b19")
+lambda_07 <- c("b5", "b10", "b15", "b20")
+
+sim_fit_all_unnest <- sim_fit_all_unnest %>% 
+  mutate(par_type = case_when(label %in% lambda_03 ~ "l03",
+                              label %in% lambda_04 ~ "l04",
+                              label %in% lambda_05 ~ "l05",
+                              label %in% lambda_06 ~ "l06",
+                              label %in% lambda_07 ~ "l07",
+                              label == "s1" ~ "s1"))
+
 # A2/A3 Number of nonconverged cases per 1000 replications -------------------
 library(kableExtra)
 #!!!!!!!! ATTENTION NORMAL AND NON-NORMAL ARE SWITCHED AROUND
@@ -302,7 +320,7 @@ fig_a9_dat <- sim_fit_cov %>%
   group_by(estimator, rep, scenario_id, label) %>% 
   mutate(bias = mean(est, na.rm = TRUE) - true_par) %>% 
   group_by(par_type, sym, N, estimator, cat) %>% 
-  summarize(bias = mean(bias, na.rm = TRUE))
+  summarize(bias = mean(bias, na.rm = TRUE) %>% round(digits = 3))
 
 
 fig_a9_dat_wide <- fig_a9_dat %>% 
@@ -356,14 +374,14 @@ kbl(fig_a9_dat_wide %>% ungroup(),
 
 
 # A10 Efficiency, Model 1, Underlying Distribution = Normal ---------------
-fig_a10_dat <- sim_fit_all_unnest2 %>% 
+fig_a10_dat <- sim_fit_all_unnest %>% 
   dplyr::filter(models == "model1") %>% 
   dplyr::filter(dist == "normal") %>% 
   group_by(estimator, scenario_id, label) %>% 
   mutate(square_diff = (est - mean(est, na.rm = TRUE))^2) %>% 
   mutate(efficiency = sqrt(sum(square_diff, na.rm = TRUE)/(n()-1))) %>% 
   group_by(par_type, sym, N, estimator, cat) %>% 
-  summarize(efficiency = mean(efficiency, na.rm = TRUE))
+  summarize(efficiency = mean(efficiency, na.rm = TRUE) %>% round(digits = 3))
 
 
 fig_a10_dat_wide <- fig_a10_dat %>% 
@@ -417,9 +435,184 @@ kbl(fig_a10_dat_wide %>% ungroup(),
 
 # A11 Efficiency, Model 1, Underlying Distribution = non-Normal ---------------
 
+fig_a11_dat <- sim_fit_all_unnest %>% 
+  dplyr::filter(models == "model1") %>% 
+  dplyr::filter(dist == "non-normal") %>% 
+  group_by(estimator, scenario_id, label) %>% 
+  mutate(square_diff = (est - mean(est, na.rm = TRUE))^2) %>% 
+  mutate(efficiency = sqrt(sum(square_diff, na.rm = TRUE)/(n()-1))) %>% 
+  group_by(par_type, sym, N, estimator, cat) %>% 
+  summarize(efficiency = mean(efficiency, na.rm = TRUE) %>% round(digits = 3))
+
+
+fig_a11_dat_wide <- fig_a11_dat %>% 
+  pivot_wider(names_from = c(sym, N, estimator),
+              values_from = c(efficiency))
+
+fig_a11_dat_wide$par_type <-  c("lambda = .3", rep(" ", 5), 
+                                "lambda = .4", rep(" ", 5),
+                                "lambda = .5", rep(" ", 5), 
+                                "lambda = .6", rep(" ", 5), 
+                                "lambda = .7", rep(" ", 5), 
+                                "phi = .3", rep(" ", 5))
+
+
+kbl(fig_a11_dat_wide %>% ungroup(),
+    col.names = c("param.", "cats", rep(c("ML", "ULS"), 20))) %>%
+  kable_classic() %>% 
+  add_header_above(c(" " = 2, 
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2,
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2,
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2,
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2,
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2)) %>% 
+  add_header_above(c(" " = 2, 
+                     "Ext. Asym." = 8,
+                     "Ext. Asym.-Alt" = 8,
+                     "Mod. Asym." = 8,
+                     "Mod. Asym.-Alt" = 8,
+                     "Symmetric" = 8)) %>%
+  footnote(general = "Mod.Asym = Moderate Asymmetry ;
+           Mod.Asym-Alt = Moderate Asymmetry-Alternating;
+           Ext. Asym = Extreme Asymmetry;
+           Ext. Asym-Alt = Extreme Asymetry-Alternating;
+           ML = robust normal-theory maximum likelihood;
+           ULS = robust categorical least squares.")
 
 
 # A12 Efficiency, Model 2, Underlying Distribution = Normal ---------------
+fig_a12_dat <- sim_fit_all_unnest %>% 
+  dplyr::filter(models == "model2") %>% 
+  dplyr::filter(dist == "normal") %>% 
+  group_by(estimator, scenario_id, label) %>% 
+  mutate(square_diff = (est - mean(est, na.rm = TRUE))^2) %>% 
+  mutate(efficiency = sqrt(sum(square_diff, na.rm = TRUE)/(n()-1))) %>% 
+  group_by(par_type, sym, N, estimator, cat) %>% 
+  summarize(efficiency = mean(efficiency, na.rm = TRUE) %>% round(digits = 3))
+
+
+fig_a12_dat_wide <- fig_a12_dat %>% 
+  pivot_wider(names_from = c(sym, N, estimator),
+              values_from = c(efficiency))
+
+fig_a12_dat_wide$par_type <-  c("lambda = .3", rep(" ", 5), 
+                                "lambda = .4", rep(" ", 5),
+                                "lambda = .5", rep(" ", 5), 
+                                "lambda = .6", rep(" ", 5), 
+                                "lambda = .7", rep(" ", 5), 
+                                "phi = .3", rep(" ", 5))
+
+
+kbl(fig_a12_dat_wide %>% ungroup(),
+    col.names = c("param.", "cats", rep(c("ML", "ULS"), 20))) %>%
+  kable_classic() %>% 
+  add_header_above(c(" " = 2, 
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2,
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2,
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2,
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2,
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2)) %>% 
+  add_header_above(c(" " = 2, 
+                     "Ext. Asym." = 8,
+                     "Ext. Asym.-Alt" = 8,
+                     "Mod. Asym." = 8,
+                     "Mod. Asym.-Alt" = 8,
+                     "Symmetric" = 8)) %>%
+  footnote(general = "Mod.Asym = Moderate Asymmetry ;
+           Mod.Asym-Alt = Moderate Asymmetry-Alternating;
+           Ext. Asym = Extreme Asymmetry;
+           Ext. Asym-Alt = Extreme Asymetry-Alternating;
+           ML = robust normal-theory maximum likelihood;
+           ULS = robust categorical least squares.")# %>% landscape() 
+
+
 
 # A13 Efficiency, Model 2, Underlying Distribution = non-Normal ---------------
-sd()
+fig_a13_dat <- sim_fit_all_unnest %>% 
+  dplyr::filter(models == "model2") %>% 
+  dplyr::filter(dist == "non-normal") %>% 
+  group_by(estimator, scenario_id, label) %>% 
+  mutate(square_diff = (est - mean(est, na.rm = TRUE))^2) %>% 
+  mutate(efficiency = sqrt(sum(square_diff, na.rm = TRUE)/(n()-1))) %>% 
+  group_by(par_type, sym, N, estimator, cat) %>% 
+  summarize(efficiency = mean(efficiency, na.rm = TRUE) %>% round(digits = 3))
+
+
+fig_a13_dat_wide <- fig_a13_dat %>% 
+  pivot_wider(names_from = c(sym, N, estimator),
+              values_from = c(efficiency))
+
+fig_a13_dat_wide$par_type <-  c("lambda = .3", rep(" ", 5), 
+                                "lambda = .4", rep(" ", 5),
+                                "lambda = .5", rep(" ", 5), 
+                                "lambda = .6", rep(" ", 5), 
+                                "lambda = .7", rep(" ", 5), 
+                                "phi = .3", rep(" ", 5))
+
+
+kbl(fig_a13_dat_wide %>% ungroup(),
+    col.names = c("param.", "cats", rep(c("ML", "ULS"), 20))) %>%
+  kable_classic() %>% 
+  add_header_above(c(" " = 2, 
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2,
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2,
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2,
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2,
+                     "N = 100" = 2,
+                     "N = 150" = 2,
+                     "N = 350" = 2,
+                     "N = 600" = 2)) %>% 
+  add_header_above(c(" " = 2, 
+                     "Ext. Asym." = 8,
+                     "Ext. Asym.-Alt" = 8,
+                     "Mod. Asym." = 8,
+                     "Mod. Asym.-Alt" = 8,
+                     "Symmetric" = 8)) %>%
+  footnote(general = "Mod.Asym = Moderate Asymmetry ;
+           Mod.Asym-Alt = Moderate Asymmetry-Alternating;
+           Ext. Asym = Extreme Asymmetry;
+           Ext. Asym-Alt = Extreme Asymetry-Alternating;
+           ML = robust normal-theory maximum likelihood;
+           ULS = robust categorical least squares.")
